9f2db2a8b63586ef35fe16620f9d1e5bdfb9bdce
shuangmin <konkacsm@gmail.com>
2015-04-10 21:06:49 +0800

    add wsgi.py

---
+++ mysite/wsgi.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+path = os.getcwd()
+print path
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
